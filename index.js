ad28d88b55dae4330e2f426d0fddae69946050db
Kushtseika Pavel <kushtseikapavel.dev@gmail.com>
2022-05-05 06:31:29 -0700

    feat: Update the books and authors

--- index.js
+++ index.js
@@ -3,28 +3,64 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   type Book {
     title: String
-    author: String
+    author: Author
+  }
+
+  type Author {
+    name: String
+    books: [Book]
   }
 
   type Query {
     books: [Book]
+    authors: [Author]
+  }
+
+  type Mutation {
+    addBook(title: String, author: String): Book
   }
 `;
 
 const books = [
   {
     title: 'The Awakening',
-    author: 'Kate Chopin',
+    author: {name: 'Kate Chopin'},
   },
   {
     title: 'City of Glass',
-    author: 'Paul Auster',
+    author: {name: 'Kate Chopin'},
+  },
+  {
+    title: 'The Pig',
+    author: {name: 'Paul Auster'},
+  },
+  {
+    title: 'The Dog',
+    author: {name: 'Paul Auster'},
   },
 ];
+
+const authors = [
+  {
+    name: 'Kate Chopin',
+    books: [
+      {title: 'City of Glass'},
+      {title: 'The Awakening'}
+    ]
+  },
+  {
+    name: 'Paul Auster',
+    books: [
+      {title: 'The Pig'},
+      {title: 'The Dog'}
+    ]
+  }
+]
 
 const resolvers = {
   Query: {
     books: () => books,
+    authors: () => authors,
   },
 };
 
